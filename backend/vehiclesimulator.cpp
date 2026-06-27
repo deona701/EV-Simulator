cbb77865518ad319a1d9cd8205b3658c7fa4c24e
deona701 <deonaleard@gmail.com>
2026-06-27 23:17:51 +0200

    changed speed so it also takes in account throttle and braking

--- backend/vehiclesimulator.cpp
+++ backend/vehiclesimulator.cpp
@@ -39,18 +39,29 @@ void VehicleSimulator::setSoc(double newSoc)
 
 void VehicleSimulator::updateSimulation()
 {
-    float friction = 18.6;
+    float natural_friction = 0.5;
+    float force = 0.0;
     double currentSoc = soc();
     double currentSpeed = speed();
+    float max_acceleration = 4.2;
+    float max_brakingForce = 12.0;
+
     float drain = 0.1 + (currentSpeed * 0.02);
     currentSoc -= drain;
 
     if (currentSoc > 0) {
-        currentSpeed += 4.2;
+        if (m_throttle >= 0.0 && m_throttle <= 1.0 && m_brake >= 0.0 && m_brake <= 1.0) {
+            /* basically max throttle gives full acceleration, half throttle gives half acceleration, but we also
+             * have natural drag which we add in every calculation to make things more realistic.
+             * we also have a maximum braking force, which makes the car brake faster.
+             */
+            force = (max_acceleration * m_throttle) - max_brakingForce * m_brake - natural_friction;
+            currentSpeed = currentSpeed + force;
+        }
     }
     if (currentSoc < 0) {
         currentSoc = 0;
-        currentSpeed = currentSpeed - friction;
+        currentSpeed = currentSpeed - 18.6;
     }
 
     currentSpeed = std::clamp(currentSpeed, 0.0, 220.0);
@@ -58,4 +69,7 @@ void VehicleSimulator::updateSimulation()
 
     setSoc(currentSoc);
     setSpeed(currentSpeed);
+
+    m_acceleration = force;
+    emit accelerationChanged();
 }
